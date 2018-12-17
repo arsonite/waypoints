14bcf567edc521608f0eebdc92e33241886dffbf
Lena <lenas.schuster@gmail.com>
2018-12-17 09:27:58 +0100

    Initial Commit

---
+++ KaisersWegpunkte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KaisersWegpunkte
+//
+//  Created by Lena Schuster on 17.12.18.
+//  Copyright © 2018 Lena Schuster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
